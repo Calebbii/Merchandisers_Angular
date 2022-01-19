f75f5896af4a077362f31454c0c8ce55e9b79a25
Calebbii <biicaleb316@gmail.com>
2022-01-20 02:35:36 +0300

    adds manager component as declarations

--- MERCHANDISERS/src/app/app.module.ts
+++ MERCHANDISERS/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './pages/nav/nav/nav.component';
+import { ManagerComponent } from './pages/manager/manager.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    ManagerComponent
   ],
   imports: [
     BrowserModule,
