4bfcfdf43ac3e809e81b0bf6de8be49562d508f9
simonkairu <simon.mureithi@student.moringaschool.com>
2022-01-20 08:10:48 +0300

    first commit

--- MERCHANDISERS/src/app/app.module.ts
+++ MERCHANDISERS/src/app/app.module.ts
@@ -4,13 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login/login.component';
+import { RegisterComponent } from './register/register/register.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+   
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
