468fcc56fd3ed2c44aee03d9daec06e21f4a3c8d
Calebbii <biicaleb316@gmail.com>
2022-01-26 10:27:59 +0300

    commit updates the navbar section

--- MERCHANDISERS/src/app/pages/user/user.component.ts
+++ MERCHANDISERS/src/app/pages/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Merchandisers } from 'src/app/models/merchandisers';
 
 @Component({
   selector: 'app-user',
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
+
+  merchandisers:Merchandisers[] = [
+    {id:1,firstname:'Caleb',lastname:'Bii',email:'cal@gmail.com',profile_image:'',contact:+25475674546},
+    {id:2,firstname:'Simon',lastname:'Murithi',email:'sim@gmail.com',profile_image:'',contact:+25478675474},
+    {id:3,firstname:'Ian',lastname:'Wanarua',email:'ian@gmail.com',profile_image:'',contact:+25475674546},
+    {id:4,firstname:'Willard',lastname:'Sigei',email:'will@gmail.com',profile_image:'',contact:+25475674546},
+    {id:5,firstname:'Muhamed',lastname:'Abdi',email:'muham@gmail.com',profile_image:'',contact:+25475674546},
+    {id:6,firstname:'Mercy',lastname:'Meni',email:'mercy@gmail.com',profile_image:'',contact:+25475674546},
+  ];
 
   constructor() { }
 
