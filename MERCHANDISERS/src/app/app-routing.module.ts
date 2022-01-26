3f73614740fe36034ad7d145c1c0be8f341aff3e
Calebbii <biicaleb316@gmail.com>
2022-01-27 01:06:40 +0300

    commit adds route

--- MERCHANDISERS/src/app/app-routing.module.ts
+++ MERCHANDISERS/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { UserComponent } from './pages/user/user.component';
 
 const routes: Routes = [
   { path: 'userprofile', component: UserComponent},
+  { path: 'dashboard', component: DashboardComponent},
 ];
 
 @NgModule({
