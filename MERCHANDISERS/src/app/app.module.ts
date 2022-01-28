fcc14415a0a83b8acd2ed447a39fc247b68b50ff
simonkairu <simon.mureithi@student.moringaschool.com>
2022-01-28 13:06:33 +0300

    update ragister and login

--- MERCHANDISERS/src/app/app.module.ts
+++ MERCHANDISERS/src/app/app.module.ts
@@ -15,7 +15,7 @@ import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
-   
+    
     LoginComponent,
     RegisterComponent,
     NavComponent,
