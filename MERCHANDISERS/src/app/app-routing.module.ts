1d33bf727c2cf92bcaa7e3d19eaf3ef4e338c681
ianwanarua <ian.wanarua@student.moringaschool.com>
2022-01-30 17:38:41 +0300

    Updated on routes

--- MERCHANDISERS/src/app/app-routing.module.ts
+++ MERCHANDISERS/src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login/login.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ManagerComponent } from './pages/manager/manager.component';
 import { UserComponent } from './pages/user/user.component';
+import { RegisterComponent } from './register/register/register.component';
 
 const routes: Routes = [
   { path: 'userprofile', component: UserComponent},
   { path: 'dashboard', component: DashboardComponent},
   { path: 'managerprofile', component: ManagerComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegisterComponent},
 ];
 
 @NgModule({
